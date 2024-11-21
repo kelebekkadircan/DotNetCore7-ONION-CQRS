4aa4eceaa076bd122482bf27c1be0b369c0bb4ab
KADIRCAN KELEBEK <kelebekkadircan@gmail.com>
2024-11-21 19:43:17 +0300

    ilk 6 video kodlandı

---
+++ Infrastructure/HepsiApi.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using Bogus;
+using HepsiApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(256);
+
+            Faker faker = new("tr");
+
+            Category category1 = new()
+            {
+                Id=1,
+                ParentId = 0,
+                Name = "Elektrik",
+                Priority = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            }; 
+
+            Category category2 = new()
+            {
+                Id = 2,
+                ParentId = 0,
+                Name = "Moda",
+                Priority = 2,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            };
+
+            Category parent1 = new()
+            {
+                Id = 3,
+                ParentId = 1,
+                Name = "Bilgisayar",
+                Priority = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            }; 
+            Category parent2 = new()
+            {
+                Id = 4,
+                ParentId = 2,
+                Name = "Kadın",
+                Priority = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            };
+
+
+            builder.HasData(category1, category2, parent1, parent2);
+
+
+
+        }
+    }
+}
--- Presentation/HepsiApi.WEBAPI/Program.cs
+++ Presentation/HepsiApi.WEBAPI/Program.cs
@@ -1,3 +1,5 @@
+using HepsiApi.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+    ;
+
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
