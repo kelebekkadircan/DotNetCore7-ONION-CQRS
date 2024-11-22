d28156cf94dccaf4418537d2080d76a16582fbba
KADIRCAN KELEBEK <kelebekkadircan@gmail.com>
2024-11-22 22:30:44 +0300

    repositories added

---
+++ Infrastructure/HepsiApi.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,81 @@
+﻿using HepsiApi.Application.Interfaces.Repositories;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+using HepsiApi.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HepsiApi.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepositories<T> where T : class, IEntityBase, new()
+    {
+        private readonly DbContext _dbContext;
+        public ReadRepository(DbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        private DbSet<T> Table => _dbContext.Set<T>();
+        public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false)
+        {
+            IQueryable<T> queryable = Table;
+
+            if (!enableTracking)  queryable = queryable.AsNoTracking();
+            
+            if(include is not  null) queryable = include(queryable);
+            
+            if(predicate is not null) queryable = queryable.Where(predicate);
+            
+            if(orderBy is not null) return await  orderBy(queryable).ToListAsync();
+            
+            return await queryable.ToListAsync();
+          
+        }
+        public async Task<IList<T>> GetAllAsyncByPaging(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false, int currentPage = 1, int pageSize = 3)
+        {
+
+            IQueryable<T> queryable = Table;
+
+            if (!enableTracking) queryable = queryable.AsNoTracking();
+
+            if (include is not null) queryable = include(queryable);
+
+            if (predicate is not null) queryable = queryable.Where(predicate);
+
+            if (orderBy is not null) return await orderBy(queryable).Skip((currentPage -1) * pageSize).Take(pageSize).ToListAsync();
+
+            return await orderBy(queryable).Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
+
+
+
+        }
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false)
+        {
+            IQueryable<T> queryable = Table;
+
+            if (!enableTracking) queryable = queryable.AsNoTracking();
+
+            if (include is not null) queryable = include(queryable);
+
+            // queryable.Where(predicate);
+
+
+            return await queryable.FirstOrDefaultAsync(predicate);
+        }
+        public async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+        {
+            Table.AsNoTracking();
+            if (predicate is null)  Table.Where(predicate);
+            return await Table.CountAsync();
+        }
+
+        public  IQueryable<T> Find(Expression<Func<T, bool>> predicate , bool enableTracking = false)
+        {
+            if(!enableTracking) Table.AsNoTracking();
+            return   Table.Where(predicate);
+
+        }
+
+
+
+    }
+}
