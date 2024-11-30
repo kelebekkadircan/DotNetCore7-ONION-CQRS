01d78fef654480d8f1292be2c185758143e3712d
KADIRCAN KELEBEK <kelebekkadircan@gmail.com>
2024-11-30 19:44:43 +0300

    redis added

---
+++ Core/HepsiApi.Application/Behaviors/RedisCacheBehavior.cs
@@ -0,0 +1,82 @@
+﻿using HepsiApi.Application.Interfaces.RedisCache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Behaviors
+{
+    public class RedisCacheBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRedisCacheService _redisCacheService;
+
+        public RedisCacheBehavior(IRedisCacheService redisCacheService)
+        {
+            _redisCacheService = redisCacheService;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request is ICacheableQuery query)
+            {
+                string cacheKey = query.CacheKey;
+                double cacheTime = query.CacheTime;
+
+                var cachedData = await _redisCacheService.GetAsync<TResponse>(cacheKey);
+                if (cachedData is not null)
+                    return cachedData;
+
+                var response = await next();
+                if (response is not null)
+                    await _redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+            }
+            return await next();
+
+        }
+    }
+
+
+    //public class RedisCacheBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    //{
+    //    private readonly IRedisCacheService _redisCacheService;
+
+    //    public RedisCacheBehavior(IRedisCacheService redisCacheService)
+    //    {
+    //        _redisCacheService = redisCacheService;
+    //    }
+
+    //    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    //    {
+    //        // Önbelleklenebilir bir sorgu mu?
+    //        if (request is ICacheableQuery query)
+    //        {
+    //            // Önbellek anahtarı ve süresi alınıyor
+    //            string cacheKey = query.CacheKey ?? throw new ArgumentNullException(nameof(query.CacheKey), "Cache anahtarı geçerli olmalıdır.");
+    //            double cacheTime = query.CacheTime;
+
+    //            // Redis'ten veri getiriliyor
+    //            var cachedData = await _redisCacheService.GetAsync<TResponse>(cacheKey);
+    //            if (cachedData is not null)
+    //            {
+    //                return cachedData; // Önbellekten döndür
+    //            }
+
+    //            // İşlem yap ve sonucu Redis'e yaz
+    //            var response = await next();
+    //            if (response is not null)
+    //            {
+    //                                await _redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+    //            }
+    //            return response;
+    //        }
+
+    //        // Önbelleklenemeyen durumlarda devam et
+    //        return await next();
+    //    }
+    //}
+
+
+}
--- Infrastructure/HepsiApi.Infrastructure/Registration.cs
+++ Infrastructure/HepsiApi.Infrastructure/Registration.cs
@@ -1,4 +1,6 @@
-﻿using HepsiApi.Application.Interfaces.Tokens;
+﻿using HepsiApi.Application.Interfaces.RedisCache;
+using HepsiApi.Application.Interfaces.Tokens;
+using HepsiApi.Infrastructure.RedisCache;
 using HepsiApi.Infrastructure.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +20,10 @@ namespace HepsiApi.Infrastructure
         {
             services.Configure<TokenSettings>(configuration.GetSection("JWT"));
             services.AddTransient<ITokenService, TokenService>();
+
+            services.Configure<RedisCacheSettings>(configuration.GetSection("RedisCacheSettings"));
+            services.AddTransient<IRedisCacheService, RedisCacheService>();
+
 
             services.AddAuthentication(opt =>
             {
@@ -39,6 +45,12 @@ namespace HepsiApi.Infrastructure
                     ClockSkew = TimeSpan.Zero
 
                 };
+            });
+
+            services.AddStackExchangeRedisCache(opt =>
+            {
+                opt.Configuration = configuration["RedisCacheSettings:ConnectionString"];
+                opt.InstanceName = configuration["RedisCacheSettings:InstanceName"];
             });
 
 
